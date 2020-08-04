3b12c822e3ca5357c9c16623d3d381f7e8e80f34
Spodorrmon <60704106+Spodorrmon@users.noreply.github.com>
2020-08-04 23:36:26 +0200

    y

--- download-test - Copy/script.js
+++ download-test - Copy/script.js
@@ -1,2 +1,12 @@
-let mousrCursor = document.querySelector(".cusor");
+let mousrCursor = document.querySelector(".cursor");
+
+let nevliks = document.querySelectorAll(".nev-links nav")
+
+
+window.addEventListener("cursormove", cursor)
+
+function cursor(e){
+    mousrCursor.style.top = e.pageY + "px"
+    mousrCursor.style.left = e.pageX + "px"
+}
 
