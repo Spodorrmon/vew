45b260b48c044552024d842ad75e92f7ba2d45b3
Spodorrmon <60704106+Spodorrmon@users.noreply.github.com>
2020-08-06 13:19:43 +0200

    i

--- download-test - Copy/script.js
+++ download-test - Copy/script.js
@@ -1,12 +1,11 @@
 let mousrCursor = document.querySelector(".mouse");
 
-let nevliks = document.querySelectorAll(".nev-links nav")
 
 
-window.addEventListener("mousemove", mouse)
+window.addEventListener("mousemove", mouse);
 
 function mouse(e){
-    console.log(e)
+    
     mousrCursor.style.top = e.pageY + "px"
     mousrCursor.style.left = e.pageX + "px"
 }
